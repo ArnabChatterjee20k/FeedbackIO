cdd65053000db02f12d65c53340645e5f64777b1
Arnab20k-Peakflo <arnab.chatterjee@peakflo.co>
2024-10-15 15:59:35 +0530

    feat -- added banners and project dashboard

---
+++ app/(dashboard)/dashboard/components/banner.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { PlayCircle } from "lucide-react";
+
+export default function Banner() {
+  return (
+    <div className="bg-gradient-to-b from-gray-300 to-gray-400 p-6 rounded-lg w-full">
+      <div className="flex flex-col md:flex-row gap-8">
+        <div className="flex-1 flex flex-col justify-center">
+          <h2 className="text-2xl font-bold mb-4">Have 5 mins?</h2>
+          <p className="text-gray-700 mb-4 flex flex-col items-start leading[1.5rem]">
+            <span className="text-xl font-medium">Here's a quick demo 👉 </span>
+            <span>
+              The demo includes an end-to-end demonstration of creating and
+              collecting testimonials.
+            </span>
+          </p>
+          <div className="space-x-4">
+            <Button variant="default" className="bg-gray-800 hover:bg-gray-700">
+              <PlayCircle className="mr-2 h-4 w-4" />
+              Watch Demo
+            </Button>
+            <Button variant="link" className="text-gray-700">
+              Learn more
+            </Button>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            <div className="flex items-center justify-center h-48 bg-gray-100 rounded">
+              <PlayCircle className="h-16 w-16 text-gray-400" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -3,14 +3,19 @@ import { signOut } from "@/lib/server/oauth";
 import { getUser } from "@/lib/server/utils";
 import { redirect } from "next/navigation";
 import Signout from "./Signout";
+import Banner from "./components/banner";
 
 export default async function page() {
   const user = await getUser();
 
   return (
     <div>
-      {user?.name}
-      <Signout/>
+      <section className="py-5">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-center">
+          <Banner />
+        </div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"></div>
+      </section>
     </div>
   );
 }
