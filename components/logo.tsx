8406b6b2fe5bd578d1a126b8ccd429a43a3f9cce
Arnab20k-Peakflo <arnab.chatterjee@peakflo.co>
2024-10-26 11:58:07 +0530

    changed the logo color schema

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,9 @@
-import {Heart} from "lucide-react"
+import { Heart } from "lucide-react";
+
 export default function Logo() {
-    return (
-      <>
-        <div className="relative h-10 w-10 bg-gradient-to-b from-[#EA580C] to-[#DEA78B] rounded-full flex">
-        <Heart width={32} className="fill-gray-800 absolute top-[0.65rem] left-1" stroke="transparent"/>
-        </div>
-      </>
-    );
-  }
+  return (
+    <div className="relative h-10 w-10 bg-gradient-to-b from-[#333333] to-[#000000] rounded-full flex items-center justify-center">
+      <Heart width={24} height={24} className="fill-white" stroke="transparent" />
+    </div>
+  );
+}
