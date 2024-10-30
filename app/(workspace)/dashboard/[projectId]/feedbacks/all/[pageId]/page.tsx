08c0518354d607eab853f222fed2c8af498e4960
Arnab20k-Peakflo <arnab.chatterjee@peakflo.co>
2024-10-30 14:53:45 +0530

    added masonry layout

--- app/(workspace)/dashboard/[projectId]/feedbacks/all/[pageId]/page.tsx
+++ app/(workspace)/dashboard/[projectId]/feedbacks/all/[pageId]/page.tsx
@@ -3,19 +3,31 @@ import { Feedbackcard } from "../../../components/feedback-card";
 import PaginatedView from "../../component/paginated-view";
 import { getAllFeedbacks } from "@/lib/server/db/feedback";
 
-export default async function page({params}:{params:{pageId:string,projectId:string}}) {
+export default async function page({
+  params,
+}: {
+  params: { pageId: string; projectId: string };
+}) {
   try {
-    const feedbacks = await getAllFeedbacks({space_id:params.projectId,page:parseInt(params.pageId || "1")})
+    const feedbacks = await getAllFeedbacks({
+      space_id: params.projectId,
+      page: parseInt(params.pageId || "1"),
+    });
     return (
-      <PaginatedView isNext = {feedbacks.isNext} baseLink={`dashboard/${params.projectId}/feedbacks/all`}>
-        <section className="grid grid-cols-4 gap-3">
+      <PaginatedView
+        isNext={feedbacks.isNext}
+        baseLink={`dashboard/${params.projectId}/feedbacks/all`}
+      >
+        <section className="columns-1 sm:columns-2 md:columns-3 lg:columns-4">
           {feedbacks.documents.map(({feedback,stars,userEmail,name})=>{
-            return <Feedbackcard email={userEmail} feedback={feedback} stars={stars} name={name}/>
+            return <div className="mb-2">
+              <Feedbackcard email={userEmail} feedback={feedback} stars={stars} name={name}/>
+            </div>
           })}
         </section>
       </PaginatedView>
     );
   } catch (error) {
-    return <h1>Some error occured</h1>
+    return <h1>Some error occured</h1>;
   }
 }
